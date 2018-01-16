b8ac0efaa93e8a64576a6b585b0ecfbc4696e21f
sam1610 <samir.amri@gmail.com>
2018-01-16 11:53:04 +0300

    upload all

---
+++ src/providers/sweather/sweather.ts
@@ -0,0 +1,15 @@
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+@Injectable()
+export class SweatherProvider {
+  url: string;
+apikey ='59d61e5d23ee4b65';
+  constructor(public http: Http) {
+    this.url='http://api.wunderground.com/api/'+this.apikey+'/conditions/q/';
+  }
+getWeather(city, state)
+{
+  return this.http.get(this.url + '/'+state +'/' +city +'.json').map(res => res.json());
+}
+}
