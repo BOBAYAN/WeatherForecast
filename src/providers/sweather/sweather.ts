77841ac893d8e03aa95dd17da09606abb32948d0
sam1610 <mlk.mimi1410@gmail.com>
2018-01-22 23:25:20 +0300

    update

--- src/providers/sweather/sweather.ts
+++ src/providers/sweather/sweather.ts
@@ -10,6 +10,7 @@ export class SweatherProvider {
 apikey ='59d61e5d23ee4b65';
   constructor(public http: Http) {
     this.url='http://api.wunderground.com/api/'+this.apikey+'/conditions/q/';
+    console.log(this.url);
   }
 getWeather(city, state)
 {
